b1bfc5359f992b6ac7cadb05679da369c3848e22
msasho <masaya.sasho@gmail.com>
2024-07-25 18:16:36 +0900

    fix databaseURI

--- parse-api/src/server.ts
+++ parse-api/src/server.ts
@@ -4,15 +4,21 @@ import { ParseServer } from 'parse-server';
 const app = express();
 
 const parseServer = new ParseServer({
-  databaseURI: 'mongodb://localhost:27017/dev',
-  appId: 'myAppId',
-  masterKey: 'myMasterKey',
+  databaseURI: 'mongodb://127.0.0.1:50003/test',
+  appId: 'APPLICATION_ID',
+  masterKey: 'MASTER_KEY',
   serverURL: 'http://localhost:1337/parse'
 });
 
 app.use('/parse', parseServer.app);
 
 const port = 1337;
-app.listen(port, () => {
-  console.log(`Parse Server running on port ${port}`);
+
+// サーバーの起動を待つ
+parseServer.start().then(() => {
+  app.listen(port, () => {
+    console.log(`Parse Server running on port ${port}`);
+  });
+}).catch((error) => {
+  console.error('Failed to start Parse Server:', error);
 });
